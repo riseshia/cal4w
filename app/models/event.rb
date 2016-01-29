6cf36fb6851d39d9a8f439402034e93017fac068
Shia <rise.shia@gmail.com>
2016-01-29 21:49:55 +0900

    add active record validation test about Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :members, class_name: 'User', foreign_key: 'user_id'
+
+  validates :subject, presence: true
+  validates :place, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Active Record Validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:subject) }
+    it { should validate_presence_of(:place) }
+  end
+
 end
