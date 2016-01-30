287d45f430f2f2ff3878be786034ae07404eabc2
Shia <rise.shia@gmail.com>
2016-01-30 17:06:16 +0900

    remove unique index from email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130052917) do
+ActiveRecord::Schema.define(version: 20160130080319) do
 
   create_table "events", force: :cascade do |t|
     t.string   "subject"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20160130052917) do
     t.string   "nickname"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["uid"], name: "index_users_on_uid"
