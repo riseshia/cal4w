53197df17c07fcdd03e4ac7054a76eb6018e23e1
Shia <rise.shia@gmail.com>
2016-02-19 18:00:00 +0900

    close #18. add Error handling when fail to post IM message

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,8 @@ class Users::SessionsController < Devise::SessionsController
   # before_filter :configure_sign_in_params, only: [:create]
   skip_before_action :authenticate_user!
 
+  rescue_from Slack::Web::Api::Error, with: :fail_to_post_im
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -10,7 +12,7 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     if params[:nickname].present?
-      send_token params[:nickname] 
+      send_token params[:nickname]
       redirect_to user_session_path, notice: 'link is sent to verify you! please check WeirdSlack!'
     else
       redirect_to user_session_path
@@ -25,13 +27,16 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def send_token(nickname)
-    client = Slack::Web::Client.new
     user = User.create_via_slack(nickname)
-    client.chat_postMessage(channel: nickname, text: "Please click next link. This token only valid 10 minutes! #{callbacks_slack_url(name: user.nickname, token: user.token)}", as_user: true, username: 'Cal4Weirdx')
+    Slack::Web::Client.new.chat_postMessage(channel: nickname, text: "Please click next link. This token only valid 10 minutes! #{callbacks_slack_url(name: user.nickname, token: user.token)}", as_user: true, username: 'Cal4Weirdx')
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
+
+  def fail_to_post_im
+    redirect_to user_session_path, notice: 'Nickname is not valid!'
+  end
 end
