8abf1b81b7612ea94ea50ff48ea99eaf6721fb6b
Shia <rise.shia@gmail.com>
2016-02-27 17:15:35 +0900

    add more options to fullcalendar to show more information

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,6 +1,12 @@
 $(document).on('ready page:load', function (event) {
   $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
     events: '/events/',
-    timezone: 'local'
+    timezone: 'local',
+    aspectRatio: 2.5
   });
 });
