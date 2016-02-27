848fbe07af7046f810e9ee66bf2bb1f3368ed5ab
Shia <rise.shia@gmail.com>
2016-02-27 16:39:27 +0900

    close #25. apply rememberable to sign in via CallbacksController#slack

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -8,6 +8,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def slack
     @user = User.from_slack(params[:name], params[:token])
     if @user
+      @user.remember_me = true
       sign_in_and_redirect @user
     else
       redirect_to user_session_path
