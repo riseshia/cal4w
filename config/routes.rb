a0078516a3e5443d3751fec17a2401107ca09e9b
Shia <rise.shia@gmail.com>
2016-03-09 23:01:06 +0900

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'events#index'
 
-  # get 'callbacks/weirdx'
-  # get 'callbacks/slack'
-
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks', registrations: 'users/registrations', sessions: 'users/sessions' }
   devise_scope :user do
     get 'callbacks/slack', to: 'callbacks#slack'
