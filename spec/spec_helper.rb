ea51e49bfef314a55ee1851a6abbe1f5a9ad632b
Shia <rise.shia@gmail.com>
2016-03-09 22:48:17 +0900

    remove formatter. it's not neccesary

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
 if ENV['CODECLIMATE_REPO_TOKEN']
   require 'codeclimate-test-reporter'
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    CodeClimate::TestReporter::Formatter
-  ]
   SimpleCov.start 'rails'
   CodeClimate::TestReporter.start
 end
