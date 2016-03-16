2b8cc92d714de25b156421b298fbd51841af4abf
Edward Kim <onward.edkim@gmail.com>
2016-03-16 23:45:56 +1100

    add locale Korean

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -10,5 +10,21 @@ $(document).on('ready page:load', function (event) {
     aspectRatio: 2.5
   });
 
-  $(".quick-datetime-select").quickDatetimeSelector();
+  $(".quick-datetime-select").quickDatetimeSelector({
+    locale: {
+      Today: '오늘',
+      Tomorrow: '내일',
+      ThisWeek: '이번 주',
+      NextWeek: '다음 주',
+      day: {
+        Sunday: '일요일',
+        Monday: '월요일',
+        Tuesday: '화요일',
+        Wednesday: '수요일',
+        Thursday: '목요일',
+        Friday: '금요일',
+        Saturday: '토요일'
+      }
+    }
+  });
 });
