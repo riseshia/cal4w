3e1bf27387f6780ce0685645498f786c8405825b
Shia <rise.shia@gmail.com>
2016-03-23 22:45:48 +0900

    update single-quote to double-quote

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,30 +1,30 @@
-$(document).on('ready page:load', function (event) {
-  $('#calendar').fullCalendar({
+$(document).on("ready page:load", function (event) {
+  $("#calendar").fullCalendar({
     header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
+      left: "prev,next today",
+      center: "title",
+      right: "month,agendaWeek,agendaDay"
     },
-    events: '/events/',
-    timezone: 'local',
+    events: "/events/",
+    timezone: "local",
     aspectRatio: 2.5
-  });
+  })
 
   $(".quick-datetime-select").quickDatetimeSelector({
     locale: {
-      Today: '오늘',
-      Tomorrow: '내일',
-      ThisWeek: '이번 주',
-      NextWeek: '다음 주',
+      Today: "오늘",
+      Tomorrow: "내일",
+      ThisWeek: "이번 주",
+      NextWeek: "다음 주",
       day: {
-        Sunday: '일요일',
-        Monday: '월요일',
-        Tuesday: '화요일',
-        Wednesday: '수요일',
-        Thursday: '목요일',
-        Friday: '금요일',
-        Saturday: '토요일'
+        Sunday: "일요일",
+        Monday: "월요일",
+        Tuesday: "화요일",
+        Wednesday: "수요일",
+        Thursday: "목요일",
+        Friday: "금요일",
+        Saturday: "토요일"
       }
     }
-  });
-});
+  })
+})
