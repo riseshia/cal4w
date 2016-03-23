98f50d9a8ebaf4c1c86047495fc12ab648f59502
Sungjin Kang <ujuc@ujuc.kr>
2016-03-23 21:39:53 +0900

    Edit vagrant bootstrap
    vagrant bootstrap 스크립트 업데이트 반영.
    아직 nokogiri 버전문제는 있음.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function install() {
-    sudo apt-get install -y "$@" #>/dev/null 2>&1
+    sudo apt-get install -y "$@" >/dev/null 2>&1
 }
 
 echo updating package information
-sudo apt-add-repository -y ppa:nginx/stable #>/dev/null 2>&1
+sudo apt-add-repository -y ppa:nginx/stable >/dev/null 2>&1
 sudo apt-get -y update #>/dev/null 2>&1
 
 # Install default packages
@@ -16,7 +16,7 @@ install tklib zlib1g-dev libssl-dev libreadline-dev libxml2 libxml2-dev \
 libxslt1-dev libmysqlclient-dev libsqlite3-dev nodejs npm git nginx
 
 # START rbenv setup #
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv #>/dev/null 2>&1
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv >/dev/null 2>&1
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build #>/dev/null 2>&1
@@ -26,15 +26,14 @@ source ~/.bashrc
 # END rbenv setup #
 
 # START ruby setup #
-~/.rbenv/bin/rbenv install 2.2.3 >/dev/null 2>&1
-~/.rbenv/bin/rbenv global 2.2.3 >/dev/null 2>&1
+~/.rbenv/bin/rbenv install 2.3.0 >/dev/null 2>&1
+~/.rbenv/bin/rbenv global 2.3.0 >/dev/null 2>&1
 ~/.rbenv/bin/rbenv rehash >/dev/null 2>&1
 ~/.rbenv/shims/ruby -v >/dev/null 2>&1
 # END ruby setup #
 
 echo installing rails >/dev/null 2>&1
 ~/.rbenv/shims/gem install bundler >/dev/null 2>&1
-~/.rbenv/shims/gem install rails -v 4.2.4 -N >/dev/null 2>&1
 ~/.rbenv/bin/rbenv rehash >/dev/null 2>&1
 
 # Add symbolic
