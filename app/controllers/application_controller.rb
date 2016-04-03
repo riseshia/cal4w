22a865c99ef8ac27853be530ff9e522b3d2e56de
Shia <rise.shia@gmail.com>
2016-04-03 16:13:26 +0900

    make less vervose

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from User::NoPermission, with: :no_permission
-
-  private
-
-  def no_permission
+  rescue_from User::NoPermission do
     redirect_to root_path, notice: "You don't have permission with this action."
   end
 end
