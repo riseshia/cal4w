23bcf61c9cdf9120d735413c95d3c5937affa51b
Shia <rise.shia@gmail.com>
2016-05-15 14:28:52 +0900

    remove unused test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,29 +10,4 @@ RSpec.describe User, type: :model do
   describe "Active Record Validations" do
     it { expect validate_presence_of(:provider) }
   end
-
-  describe "#from_slack" do
-    it "will create new user" do
-      User.from_slack("name")
-      expect change(User, :count).by(1)
-    end
-
-    it "will return exist user" do
-      user = create(:user)
-      expect(User.from_slack(user.nickname)).to eq(user)
-    end
-  end
-
-  describe "#generate_token" do
-    it "will have no token when it created" do
-      user = create(:user)
-      expect(user.token).to be_nil
-    end
-
-    it "will regenerate token" do
-      user = create(:user)
-      user.generate_token
-      expect(user.token).not_to be_nil
-    end
-  end
 end
