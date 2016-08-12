0936d873421694aebd245084b1fc041c62b9d046
Shia <rise.shia@gmail.com>
2016-08-12 09:00:20 +0900

    Show Loading Message until ajax request is done

--- app/assets/javascripts/components/event_index.js.jsx
+++ app/assets/javascripts/components/event_index.js.jsx
@@ -1,17 +1,32 @@
 class EventIndex extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { data: [] }
+    this.state = {
+      data: [],
+      isLoaded: false
+    }
   }
 
   componentDidMount() {
     const today = moment()
     const todayStr = today.format("YYYY-MM-DD")
     $.getJSON("/api/events/", {start: todayStr, _: +today}).success((data) => {
-      this.setState({data: data})
+      this.setState({data: data, isLoaded: true})
     }.bind(this))
   }
+
+  renderLoadMessage() {
+    return (
+      <div>
+        <p>이벤트 정보를 불러오고 있습니다.</p>
+      </div>
+    )
+  }
+
   render() {
+    if (!this.state.isLoaded) {
+      return this.renderLoadMessage();
+    }
     return (
       <div>
         <Calendar data={this.state.data} />
