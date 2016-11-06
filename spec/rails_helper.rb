b8bf645caceddebe4c741f8cdd43b589779112ab
Shia <rise.shia@gmail.com>
2016-11-06 12:02:42 +0900

    Add simplecov gem explicitly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 if ENV["CODECLIMATE_REPO_TOKEN"]
   require "codeclimate-test-reporter"
-  SimpleCov.start "rails"
-  CodeClimate::TestReporter.start
+  SimpleCov.start "rails" do
+    add_filter "/vendor/"
+    add_filter "/spec/"
+  end
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
