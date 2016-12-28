d506bf4a0fcfdb3b721110ea8d25f573c342f44f
Shia <rise.shia@gmail.com>
2016-12-28 22:12:41 +0900

    Remove unnecessary validate & form (#273)
    * Remove unnecessary validate
    
    * Remove EventForm

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,43 +1,39 @@
 # frozen_string_literal: true
 # EventsController
 class EventsController < ApplicationController
-  before_action :set_event, except: [:index, :new, :create]
   before_action :permission_check, only: [:edit, :update, :destroy, :copy]
 
   def index
   end
 
   def show
+    event
     @user = current_user
   end
 
   def join
-    @event.members << current_user
-    @event.notify_new_member current_user, event_url(@event)
-    redirect_to @event, notice: "Thanks to your join!"
+    event.members << current_user
+    event.notify_new_member current_user, event_url(event)
+    redirect_to event, notice: "Thanks to your join!"
   end
 
   def unjoin
-    @event.members.destroy current_user
-    @event.notify_cancel_member current_user, event_url(@event)
-    redirect_to @event, notice: "Ok, I believe we could see soon! :)"
+    event.members.destroy current_user
+    event.notify_cancel_member current_user, event_url(event)
+    redirect_to event, notice: "Ok, I believe we could see soon! :)"
   end
 
   def new
     @event = Event.new(start_time: Time.zone.now + 1.hour)
-    @event_form = EventForm.init_with_event(@event)
   end
 
   def edit
-    Time.zone = @event.timezone
-    @event_form = EventForm.init_with_event(@event)
+    Time.zone = event.timezone
   end
 
   def create
-    @event_form = EventForm.init_with_params(event_form_params)
-    @event = Event.init_with_user(@event_form.attributes, current_user)
-    if @event_form.valid?
-      @event.save!(validate: false)
+    @event = Event.init_with_user(event_params, current_user)
+    if @event.save
       @event.notify_new_event event_url(@event)
       redirect_to @event, notice: "성공적으로 밋업을 만들었습니다."
     else
@@ -47,38 +43,36 @@ class EventsController < ApplicationController
   end
 
   def update
-    Time.zone = @event.timezone
-    @event_form = EventForm.init_with_params(event_form_params)
-    if @event_form.valid?
-      @event.update_attributes(@event_form.attributes)
-      @event.notify_updated_event event_url(@event)
-      redirect_to @event, notice: "성공적으로 밋업을 변경했습니다."
+    Time.zone = event.timezone
+
+    if event.update(event_params)
+      event.notify_updated_event event_url(event)
+      redirect_to event, notice: "성공적으로 밋업을 변경했습니다."
     else
       render :edit
     end
   end
 
   def destroy
-    @event.destroy
-    @event.notify_destroyed_event unless @event.persisted? # destroyed
+    event.notify_destroyed_event unless event.destroy
     redirect_to events_url, notice: "밋업이 취소되었습니다."
   end
 
   private
 
-  def set_event
-    @event = Event.find(params[:id])
-  end
-
-  def event_form_params
+  def event_params
     params.require(:event).permit(
       :title, :place, :description,
       :start_time, :planned_time, :timezone
     )
   end
 
+  def event
+    @event ||= Event.find(params[:id])
+  end
+
   def permission_check
-    raise User::NoPermission unless @event.editable? current_user
+    raise User::NoPermission unless event.editable? current_user
   end
 
   rescue_from ActiveRecord::RecordNotFound do
