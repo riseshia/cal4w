d82441c4f25bb090b81055977d37714df5e8d567
Shia <rise.shia@gmail.com>
2017-01-04 14:17:48 +0900

    Add uniqueness validation to event_user

--- spec/models/event_user_spec.rb
+++ spec/models/event_user_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe EventUser, type: :model do
     it { expect belong_to(:user) }
     it { expect belong_to(:event) }
   end
+
+  describe "Active Record Validations" do
+    context "uniqueness check" do
+      it "returns false" do
+        user = create(:user)
+        event = create(:event)
+        event.members << user
+        event_user = EventUser.new(user_id: user.id, event_id: event.id)
+        expect(event_user.valid?).to be(false)
+      end
+    end
+  end
 end
