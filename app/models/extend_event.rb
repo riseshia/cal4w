aba7d5f0d1c7aa331a77332bec01233b1346ac51
Shia <rise.shia@gmail.com>
2017-04-03 20:35:22 +0900

    Example Implement

---
+++ app/models/extend_event.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# Event
+class ExtendEvent < ApplicationRecord
+  self.table_name = "events"
+  belongs_to :user
+
+  default_scope do
+    joins(:user).select("title", "place", "users.uid", "users.provider")
+  end
+
+  def attr
+    uid + provider
+  end
+end
